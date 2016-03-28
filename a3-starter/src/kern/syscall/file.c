72c5aa02d19123ed70b2056e5a96432151fb04fc
frankychan007 <frankychan007@gmail.com>
2016-03-28 16:39:37 -0400

    Update file.c

--- a3-starter/src/kern/syscall/file.c
+++ a3-starter/src/kern/syscall/file.c
@@ -70,6 +70,35 @@ file_close(int fd)
 int
 filetable_init(void)
 {
+	// Allocate the space of filetable and the entry of filetable
+	struct filetable *ft = (struct filetable *) kmalloc(sizeof(struct  filetable));
+	ft->ft_entries[0] = (struct ft_entry)kmalloc(sizeof(struct ft_entry));
+	
+	// Get a vnode for the * console device
+	struct vnode *cons_vnode=NULL; 
+	int = result;
+	char path[5];
+	strcpy(path, "con:");
+  	result = vfs_open(path, O_RDWR, 0, &cons_vnode);
+  	// Set up the first 3 file descriptors
+  	ft->ft_entries[0]->file_vnode = cons_vnode;
+  	ft->ft_entries[0]->flags = O_RDWR;
+  	ft->ft_entries[0]->pos = 0;
+  	ft->ft_entries[0]->count = 3;
+  	ft->ft_entries[1] = ft->ft_entries[0];
+  	ft->ft_entries[2] = ft->ft_entries[0];
+
+  	// Init all other entries to NULL
+  	int i = 3;
+  	while (i < __OPEN_MAX) {
+  		ft->ft_entries[i] = NULL;
+  		i++;
+  	}
+  	// Init the filetable spinlock
+  	spinlock_init(&ft->ft_spinlock);
+
+  	//Set curthread->t_filetable to point to the newly-initialized filetable
+  	curthread->t_filetable = ft;
 	return 0;
 }	
 
@@ -82,8 +111,16 @@ filetable_init(void)
 void
 filetable_destroy(struct filetable *ft)
 {
-        (void)ft;
-}	
+	int i = 0;
+	while(i < __OPEN_MAX) {
+		struct filetable_entry *ft_e = ft_ft_entries[i];
+		if (ft_e != NULL) {
+			file_close(i);
+		}
+	}
+	spinlock_cleanup(&ft->ft_spinlock);
+	kfree(ft);
+}		
 
 
 /* 
